1a3a6123f94dc78c2a3463a126d16aff7c51da6d
冯大大 <1105044226@qq.com>
2019-01-15 17:16:09 +0800

    vue3.0Start

---
+++ vue-porject/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <el-container>
+		  <el-header>
+		  	<HeaderMain/>
+		  </el-header>
+		  <el-container>
+		    <el-aside width="200px">左边栏</el-aside>
+		    <el-main>
+		    	<!--<router-view/>-->
+		    </el-main>
+		  </el-container>
+		</el-container>
+	</div>
+</template>
+<script>
+	import HeaderMain from '@/components/Common/HeaderMain.vue';
+	export default {
+	  components: {
+	    HeaderMain
+	  }
+	};
+</script>
+</script>
+<style lang="stylus">
+	body
+		margin 0 auto
+		font-family '微软雅黑'
+	.el-header
+		padding 0px !important;
+		background #5D9CEC
+/*#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+
+#nav
+  padding 30px
+  a
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983*/
+</style>
---
+++ vue-porject/src/components/Common/HeaderMain.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+		<div class="HeaderLogo">
+  			<img class="Logo" src="@/assets/headerLogo.png" alt="" />
+	  		<span class="LogoText">区域基本公共卫生信息管理平台</span>
+	  	</div>
+		<el-menu
+		  :default-active="activeIndex2"
+		  class="el-menu-demo"
+		  mode="horizontal"
+		  @select="handleSelect"
+		  background-color="#5D9CEC"
+		  text-color="#fff"
+		  active-text-color="#ffd04b"
+		  >
+			<el-menu-item index="1">{{timer}}</el-menu-item>
+			<el-menu-item index="2">
+				<img src="@/assets/icon/out.png" alt="" />
+			</el-menu-item>
+		</el-menu>
+	</div>
+</template>
+<script>
+    import dayjs from 'dayjs'
+    export default {
+	  	name: 'HeaderMain',
+	    data() {
+	      return {
+	        activeIndex2: '1',
+	        timer : null
+	      };
+	    },
+	    created:function(){
+	    	setInterval(this.timerFun, 1000);
+	    },
+	    methods: {
+	    	timerFun: function () {
+	            this.timer = dayjs().format('YYYY-MM-DD HH:mm:ss')
+	        },
+	        handleSelect(key, keyPath) {
+	          	console.log(key, keyPath);
+	        }
+	    }
+    }
+</script>
+
+<style scoped lang="stylus">
+	.HeaderLogo
+		float left
+		height 60px
+		line-height 60px
+	.Logo
+		width 35px
+		padding 0px 10px
+		vertical-align middle
+	.LogoText
+		font-size 20px
+		font-weight bold	
+		vertical-align: middle
+		vertical-align middle
+		color #fff
+	.el-menu
+		display inline-block
+		float right
+	.el-menu-item
+		border-bottom none !important
+</style>
